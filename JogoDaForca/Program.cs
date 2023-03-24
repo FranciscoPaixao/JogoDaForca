a93c217b4a10aec60f2e39a63e12472ff03e110e
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-24 14:57:49 -0300

    .

--- JogoDaForca/Program.cs
+++ JogoDaForca/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Jogo da Forca!");
-            String[] apenasPalavras = { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJÁ", "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA", " ", "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
+            String[] apenasPalavras = { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJÁ", "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA", "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
             Random numeroRandom = new Random();
             int escolhePalavra = numeroRandom.Next(0, apenasPalavras.Length);
             String palavraEscolhida = apenasPalavras[escolhePalavra];
@@ -16,7 +16,17 @@
             bool errou;
             while(cont < 5)
             {
+                Console.Clear();
                 Console.WriteLine($"DICA: A palavra secreta contem {palavraEscolhida.Length} letras. :3");
+                Console.WriteLine("  _______________     ");
+                Console.WriteLine(" |/             |     ");
+                Console.WriteLine(" |              |     ");
+                Console.WriteLine(" |              {0}   ", (cont >= 1 ? "o" : " ")); 
+                Console.WriteLine(" |             {0}    ", (cont >= 2 ? "/x\\" : " "));
+                Console.WriteLine(" |              {0}   ", (cont >= 3 ? "x" : "  "));
+                Console.WriteLine(" |             {0}    ", (cont >= 4 ? "/ \\" : " "));
+                Console.WriteLine(" |                    ");
+                Console.WriteLine(" |                    ");
                 Console.WriteLine("Resultado: " + new string(resultado));
                 Console.Write("Qual seu chute?");
                 chute = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
@@ -35,6 +45,8 @@
                 }
                 if(resultado.SequenceEqual(palavraEscolhida.ToCharArray()))
                 {
+                    Console.Clear();
+                    Console.WriteLine("Resultado: " + new string(resultado));
                     Console.WriteLine("Parabéns! Você venceu!");
                     break;
                 }
